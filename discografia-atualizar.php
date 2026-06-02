f489d79369d0e2bed9cb8eba65f4cff0a6d5992e
Nan Santos <pixelado405@gmail.com>
2026-06-01 22:27:16 -0300

    Agora é possível editar os discos cadastrados!

---
+++ discografia-atualizar.php
@@ -0,0 +1,17 @@
+<?php
+
+$id = $_GET['id'];
+$nome = $_POST['nome'];
+$artista = $_POST['artista'];
+$ano = $_POST['ano'];
+$tipo = $_POST['tipo'];
+$foto = $_POST['foto'];
+
+include "inc-conexao.php";
+$resultado = mysqli_execute_query($conn, "UPDATE tb_discografia SET artista = ?, nome = ?, ano = ?, tipo = ?, foto = ? WHERE id = ?", 
+[$artista, $nome, $ano, $tipo, $foto, $id]);
+
+mysqli_close($conn);
+header('Location:discografia-listagem.php');
+
+?>
--- discografia-editar.php
+++ discografia-editar.php
@@ -22,20 +22,23 @@ include "inc-cabecalho.php";
     <?php include "inc-menu.php"; ?>
     <main class='container'>
         <h1>Editar disco: <?=$nome?></h1>
-        <form method="post" action="discografia-atualizar.php?id=<?=$id?>">
-            Artista: <input name="artista" value="<?=$artista?>"> <br>
-            Nome do disco: <input name="nome" value="<?=$nome?>"> <br>
-            Ano: <input type="number" name="ano" value="<?=$ano?>"> <br>
-            Foto: <input name="foto" value="<?=$foto?>"> <br>
-            Tipo:
-            <select name="tipo">
-                <option value=""></option>
-                <option value="album">Álbum</option>
-                <option value="single">Single</option>
-            </select>
-            <br>
-            <button type="submit">Atualizar disco</button>
-        </form>
+        <div class="d-flex flex-column align-items-center">
+            <div class="card p-4">
+                <form method="post" action="discografia-atualizar.php?id=<?=$id?>">
+                    Artista: <input name="artista" value="<?=$artista?>"> <br><br>
+                    Nome do disco: <input name="nome" value="<?=$nome?>"> <br><br>
+                    Ano: <input type="number" name="ano" value="<?=$ano?>"> <br><br>
+                    Foto: <input name="foto" value="<?=$foto?>"> <br><br>
+                    Tipo:
+                    <select name="tipo">
+                        <option value="album" <?php if($tipo == 'album'){echo "selected";} ?> >Álbum</option>
+                        <option value="single" <?php if($tipo == 'single'){echo "selected";} ?> >Single</option>
+                    </select>
+                    <br><br>
+                    <button type="submit">Atualizar disco</button>
+                </form>
+            </div>
+        </div>
     </main> 
 
 <?php
